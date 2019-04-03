09ac5a45c4004872b6dd5765f0fe37099f974d39
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2019-04-02 22:25:42 -0600

    Add http

--- Makefile
+++ Makefile
@@ -8,4 +8,7 @@ BUILD_PATH=bin
 build_win:
 	$(GOBUILD) -o $(BUILD_PATH)/$(WIN_BINARY_NAME) -v
 
+deps:
+	$(GOGET) -u github.com/gorilla/mux
+
 .PHONY: build_win
--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 )
 
+func SimpleBalancer() {
+	ln, _ := net.Listen("tcp", ":80")
+
+	conn, _ := ln.Accept()
+
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+		fmt.Print("Message:", string(message))
+
+	}
+}
+
 func main() {
-	fmt.Printf("Hello, World!")
+	fmt.Printf("Starting Balancer...")
+	SimpleBalancer()
 }
